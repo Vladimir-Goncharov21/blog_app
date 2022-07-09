8b2ddf266516fbf5bed43f1aeadcb43b2a043479
vgoncharov <vgoncharov@hse.ru>
2022-07-09 13:04:39 +0300

    Добавлена форма создания

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
-    <div>
+    <div class = "app">
+        <form>
+            <h4>Создание поста</h4>
+            <input class="input" type="text" placeholder="Название">
+            <input class="input" type="text" placeholder="Описание">
+            <button class="btn">
+                Создать
+            </button>
+        </form>
         <div class="post" v-for="post in posts" :key="post.id">
-            <div><strong>Название: </strong>{{post.title}}</div>
-            <div><strong>Название: </strong>{{post.body}}</div>
+            <div><strong>Название: </strong>{{ post.title }}</div>
+            <div><strong>Название: </strong>{{ post.body }}</div>
         </div>
     </div>
 </template>
@@ -12,9 +20,9 @@ export default {
     data() {
         return {
             posts: [
-                {id: 1, title: 'JavaScript', body : 'Описание поста'},
-                {id: 2, title: 'JavaScript 2', body : 'Описание поста 2'},
-                {id: 3, title: 'JavaScript 3', body : 'Описание поста 3'}
+                { id: 1, title: 'JavaScript', body: 'Описание поста' },
+                { id: 2, title: 'JavaScript 2', body: 'Описание поста 2' },
+                { id: 3, title: 'JavaScript 3', body: 'Описание поста 3' }
             ]
         }
     }
@@ -27,11 +35,34 @@ export default {
     padding: 0;
     box-sizing: border-box;
 }
-
+.app {
+    padding: 20px;
+}
 .post {
     padding: 15px;
     border: 2px solid teal;
     margin-top: 15px;
 
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+}
+
+.btn {
+    align-self: flex-end;
+    margin-top: 15px;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 1px solid teal;
+}
+
+.input {
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px;
+    margin-top: 15px;
 }
 </style>
