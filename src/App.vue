f1f0da77e27ffdddb69635a437c72b6efae9c6aa
vgoncharov <vgoncharov@hse.ru>
2022-07-09 13:43:30 +0300

    Настроил форму создания

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
     <div class = "app">
-        <form>
+        <form @submit.prevent>
             <h4>Создание поста</h4>
-            <input class="input" type="text" placeholder="Название">
-            <input class="input" type="text" placeholder="Описание">
-            <button class="btn">
+            <input v-bind:value="title" @input="title = $event.target.value" class="input" type="text" placeholder="Название">
+            <input v-bind:value="body"  @input="body = $event.target.value" class="input" type="text" placeholder="Описание">
+            <button class="btn" @click="createPost">
                 Создать
             </button>
         </form>
@@ -23,7 +23,19 @@ export default {
                 { id: 1, title: 'JavaScript', body: 'Описание поста' },
                 { id: 2, title: 'JavaScript 2', body: 'Описание поста 2' },
                 { id: 3, title: 'JavaScript 3', body: 'Описание поста 3' }
-            ]
+            ],
+            title : '',
+            body : ''
+        }
+    },
+    methods : {
+        createPost() {
+            const newPost = {
+                id: Date.now(),
+                title: this.title,
+                body: this.body
+            }
+            this.posts.push(newPost)
         }
     }
 }
